6c58fd571514fa2a688852e9f646846c4ef144cd
miqdok <milana.horalevych@gmail.com>
2024-03-20 21:00:05 +0200

    Change code

--- main.asm
+++ main.asm
@@ -1,72 +1,63 @@
-.model small
-.stack 100h
+call    StrLength       ; Find length of target string
+    mov     ax, cx          ; Save length(s2) in ax
+    xchg    si, di          ; Swap si and di
+    call    StrLength       ; Find length of substring
+    mov     bx, cx          ; Save length(s1) in bx
+    xchg    si, di          ; Restore si and di
+    sub     ax, bx          ; ax = last possible index
+    jb      @@20            ; Exit if len target < len substring
+    mov     dx, 0ffffh      ; Initialize dx to -1
+@@10:
+    inc     dx              ; For i = 0 TO last possible index
+    mov     cl, [byte bx + di]      ; Save char at s[bx] in cl
+    mov     [byte bx + di], 0 ; Replace char with null
+    call    StrCompare              ; Compare si to altered di
+    mov     [byte bx + di], cl      ; Restore replaced char
+    je      @@20            ; Jump if match found, dx=index, zf=1
+    inc     di              ; Else advance target string index
+    cmp     dx, ax          ; When equal, all positions checked
+    jne     @@10            ; Continue search unless not found
 
-.data
-substring db 255 dup(0)                  ; Змінна для зберігання командного рядка
-substringLength db ?                     ; Змінна для зберігання довжини командного рядка
-oneChar db ?                             ; Змінна для зберігання одного символу
+    xor     cx, cx          ; Substring not found.  Reset zf = 0
+    inc     cx              ;  to indicate no match
+@@20
+    pop     di              ; Restore registers
+    pop     cx
+    pop     bx
+    pop     ax
+    ret                     ; Return to caller
+StrPos ENDP
 
-.code
-main PROC
-    mov ax, ds
-    mov es, ax                           ; Збереження сегменту PSP в ES для доступу до аргументів командного рядка
-    
-    mov ax, @data                        ; Ініціалізування сегменту даних
-    mov ds, ax
+StrLength PROC
+    push    ax              ; Save modified registers
+    push    di
 
-    xor ah, ah
-    mov al, es:[80h]                     
-    mov substringLength, al              ; Збереження довжини аргументу командного рядка
+    xor     al, al          ; al <- search char (null)
+    mov     cx, 0ffffh      ; cx <- maximum search depth
+    cld                     ; Auto-increment di
+    repnz   scasb           ; Scan for al while [di]<>null & cx<>0
+    not     cx              ; Ones complement of cx
+    dec     cx              ;  minus 1 equals string length
 
-    mov si, 82h                          ; Початок аргументу командного рядка
-    mov di, offset substring             ; Призначення(місце) аргументу командного рядка
-    mov cl, substringLength
+    pop     di              ; Restore registers
+    pop     ax
+    ret                     ; Return to caller
+StrLength ENDP
 
-copy_substring:
-    mov al, es:[si]                      ; Копіювання аргументу командного рядка до параметра 
-    mov [di], al                         ; Копіювання аргументу до масиву параметрів
-    inc si
-    inc di
-    dec cl
-    cmp cl, 0
-    jne copy_substring                   ; Повторення циклу, поки не буде скопійовано всі аргументи
-
-    xor ax, ax
-    mov di, offset substring
-    mov cl, substringLength
-print_substring:
-    mov ah, 02h                          ; Функція виводу символу
-    mov dl, [di]                         ; Завантаження символу для виводу
-    int 21h                              ; Виклик DOS interrupt
-    inc di
-    dec cl
-    cmp cl, 0
-    jne print_substring
-
-    mov dl, 0Ah                          ; Новий рядок
-    int 21h
-
-read_next:
-    mov ah, 3Fh                          ; Зчитування символа з stdin
-    mov bx, 0h                           ; stdin handle
-    mov cx, 1                            ; 1 byte to read
-    mov dx, offset oneChar               ; read to ds:dx 
-    int 21h                              ; ax = number of bytes read
-    
-    ; Перевірка на кінець файлу (EOF)
-    or ax, ax
-    jz end_program
-
-    ; Виведення зчитаного символу на stdout
-    mov ah, 02h                          ; Функція виводу символу
-    mov dl, [oneChar]                    ; Завантаження символу для виводу
-    int 21h                              ; Виклик DOS interrupt
-
-    jmp read_next                        ; Повторення процесу зчитування
-
-end_program:
-    mov ax, 4C00h                        ; Завершення програми
-    int 21h                              ; Виклик DOS interrupt
-
-main ENDP
-end main
+StrCompare PROC
+    push    ax              ; Save modified registers
+    push    di
+    push    si
+    cld                     ; Auto-increment si
+@@10:
+    lodsb                   ; al <- [si], si <- si + 1
+    scasb                   ; Compare al and [di]; di <- di + 1
+    jne     @@20            ; Exit if non-equal chars found
+    or      al, al          ; Is al=0? (i.e. at end of s1)
+    jne     @@10            ; If no jump, else exit
+@@20:
+    pop     si              ; Restore registers
+    pop     di
+    pop     ax
+    ret                     ; Return flags to caller
+StrCompare ENDP
